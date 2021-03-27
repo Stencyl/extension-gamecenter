c3f5a609fe2edd6c88b00e060f9f1eeb69750603
Justin Espedal <justin.espedal@gmail.com>
2021-03-27 15:47:05 +0900

    Import all data from toolset and engine

--- src/com/stencyl/gamecenter/GameCenter.hx
+++ src/com/stencyl/gamecenter/GameCenter.hx
@@ -1,182 +1,194 @@
 package com.stencyl.gamecenter;
 
-#if cpp
 import cpp.Lib;
-#else
-import openfl.Lib;
-#end
 
-import com.stencyl.Engine;
-import com.stencyl.event.EventMaster;
-import com.stencyl.event.StencylEvent;
+import com.stencyl.Extension;
+import com.stencyl.event.Event;
+import com.stencyl.models.Scene;
 
-import openfl.utils.ByteArray;
-import openfl.display.BitmapData;
-import openfl.geom.Rectangle;
+using com.stencyl.event.EventDispatcher;
 
-#if ios
 @:buildXml('<include name="${haxelib:com.stencyl.gamecenter}/project/Build.xml"/>')
 //This is just here to prevent the otherwise indirectly referenced native code from bring stripped at link time.
 @:cppFileCode('extern "C" int gamecenter_register_prims();void com_stencyl_gamecenter_link(){gamecenter_register_prims();}')
-#end
-class GameCenter 
-{	
+class GameCenter extends Extension
+{
+	private static var instance:GameCenter;
 	private static var initialized:Bool = false;
 	
+	//stencyl events
+	public var gcEvent:Event<(GcEventData)->Void>;
+	public var nativeEventQueue:Array<GcEventData> = [];
+	
+	public function new()
+	{
+		super();
+		instance = this;
+	}
+
+	public static function get()
+	{
+		return instance;
+	}
+
+	//Native callbacks
+
 	private static function notifyListeners(inEvent:Dynamic)
 	{
-		#if ios
 		var type:String = Std.string(Reflect.field(inEvent, "type"));
 		var data:String = Std.string(Reflect.field(inEvent, "data"));
 		
 		if(type == "auth-success")
 		{
 			trace("Game Center: Authenticated");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_READY));
+			instance.nativeEventQueue.push(GAME_CENTER_READY);
 		}
 		
 		else if(type == "auth-failed")
 		{
 			trace("Game Center: Failed to Authenticate");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_READY_FAIL, data));
+			instance.nativeEventQueue.push(GAME_CENTER_READY_FAIL);
 		}
 		
 		else if(type == "score-success")
 		{
 			trace("Game Center: Submitted Score");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_SCORE, data));
+			instance.nativeEventQueue.push(GAME_CENTER_SCORE(data));
 		}
 		
 		else if(type == "score-failed")
 		{
 			trace("Game Center: Failed to Submit Score");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_SCORE_FAIL, data));
+			instance.nativeEventQueue.push(GAME_CENTER_SCORE_FAIL(data));
 		}
 		
 		else if(type == "achieve-success")
 		{
 			trace("Game Center: Submitted Achievement");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_ACHIEVEMENT, data));
+			instance.nativeEventQueue.push(GAME_CENTER_ACHIEVEMENT(data));
 		}
 		
 		else if(type == "achieve-failed")
 		{
 			trace("Game Center: Failed to Submit Achievement");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_ACHIEVEMENT_FAIL, data));
+			instance.nativeEventQueue.push(GAME_CENTER_ACHIEVEMENT_FAIL(data));
 		}
 		
 		else if(type == "achieve-reset-success")
 		{
 			trace("Game Center: Reset Achievements");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_ACHIEVEMENT_RESET, data));
+			instance.nativeEventQueue.push(GAME_CENTER_ACHIEVEMENT_RESET);
 		}
 		
 		else if(type == "achieve-reset-failed")
 		{
 			trace("Game Center: Failed to Reset Achievements");
-			Engine.events.addGameCenterEvent(new StencylEvent(StencylEvent.GAME_CENTER_ACHIEVEMENT_RESET_FAIL, data));
+			instance.nativeEventQueue.push(GAME_CENTER_ACHIEVEMENT_RESET_FAIL);
 		}
-		#end
 	}
 
-	public static function initialize():Void 
+	//Design Mode blocks
+
+	public static function initializeGamecenter():Void 
 	{
-		#if ios
 		if(!initialized)
 		{
 			set_event_handle(notifyListeners);
 			gamecenter_initialize();
 			initialized = true;
 		}
-		#end	
 	}
 
 	public static function authenticate():Void 
 	{
-		#if ios
-			gamecenter_authenticate();
-		#end	
+		gamecenter_authenticate();
 	}
 	
 	public static function isAvailable():Bool 
 	{
-		#if ios
-			return gamecenter_isavailable();
-		#else
-			return false;
-		#end
+		return gamecenter_isavailable();
 	}
 	
 	public static function isAuthenticated():Bool 
 	{
-		#if ios
-			return gamecenter_isauthenticated();
-		#else
-			return false;
-		#end
+		return gamecenter_isauthenticated();
 	}
 	
 	public static function getPlayerName():String 
 	{
-		#if ios
-			return gamecenter_playername();
-		#else
-			return "None";
-		#end
+		return gamecenter_playername();
 	}
 	
 	public static function getPlayerID():String 
 	{
-		#if ios
-			return gamecenter_playerid();
-		#else
-			return "None";
-		#end
+		return gamecenter_playerid();
 	}
 	
 	public static function showLeaderboard(categoryID:String):Void 
 	{
-		#if ios
-			gamecenter_showleaderboard(categoryID);
-		#end	
+		gamecenter_showleaderboard(categoryID);
 	}
 	
 	public static function showAchievements():Void 
 	{
-		#if ios
-			gamecenter_showachievements();
-		#end	
+		gamecenter_showachievements();
 	}
 	
 	public static function reportScore(categoryID:String, score:Int):Void 
 	{
-		#if ios
-			gamecenter_reportscore(categoryID, score);
-		#end	
+		gamecenter_reportscore(categoryID, score);
 	}
 	
 	public static function reportAchievement(achievementID:String, percent:Float):Void 
 	{
-		#if ios
-			gamecenter_reportachievement(achievementID, percent);
-		#end	
+		gamecenter_reportachievement(achievementID, percent);
 	}
 	
 	public static function resetAchievements():Void 
 	{
-		#if ios
-			gamecenter_resetachievements();
-		#end	
+		gamecenter_resetachievements();
 	}
 	
 	public static function showAchievementBanner(title:String, message:String):Void
 	{
-		#if ios
-			gamecenter_showachievementbanner(title, message);
-		#end	
+		gamecenter_showachievementbanner(title, message);
+	}
+
+	public static function getSubjectID(eventData:GcEventData):String
+	{
+		return switch(eventData)
+		{
+			case GAME_CENTER_SCORE(categoryID): categoryID;
+			case GAME_CENTER_SCORE_FAIL(categoryID): categoryID;
+			case GAME_CENTER_ACHIEVEMENT(achievementID): achievementID;
+			case GAME_CENTER_ACHIEVEMENT_FAIL(achievementID): achievementID;
+			case _: "";
+		};
+	}
+
+	//Extension
+
+	public override function loadScene(scene:Scene)
+	{
+		gcEvent = new Event<(GcEventData)->Void>();
 	}
 	
-	#if ios
+	public override function cleanupScene()
+	{
+		gcEvent = null;
+	}
+
+	public override function preSceneUpdate()
+	{
+		for(event in nativeEventQueue)
+		{
+			gcEvent.dispatch(event);
+		}
+		nativeEventQueue.splice(0, nativeEventQueue.length);
+	}
+
+	//Function loaders
+
 	private static var set_event_handle = Lib.load("gamecenter", "gamecenter_set_event_handle", 1);
 	private static var gamecenter_initialize = Lib.load("gamecenter", "gamecenter_initialize", 0);
 	private static var gamecenter_authenticate = Lib.load("gamecenter", "gamecenter_authenticate", 0);
@@ -192,5 +204,15 @@ class GameCenter
 	private static var gamecenter_reportachievement = Lib.load("gamecenter", "gamecenter_reportachievement", 2);
 	private static var gamecenter_resetachievements = Lib.load("gamecenter", "gamecenter_resetachievements", 0);
 	private static var gamecenter_showachievementbanner = Lib.load("gamecenter", "gamecenter_showachievementbanner", 2);
-	#end
+}
+
+enum GcEventData {
+	GAME_CENTER_READY;
+	GAME_CENTER_READY_FAIL;
+	GAME_CENTER_SCORE(categoryID:String);
+	GAME_CENTER_SCORE_FAIL(categoryID:String);
+	GAME_CENTER_ACHIEVEMENT(achievementID:String);
+	GAME_CENTER_ACHIEVEMENT_FAIL(achievementID:String);
+	GAME_CENTER_ACHIEVEMENT_RESET;
+	GAME_CENTER_ACHIEVEMENT_RESET_FAIL;
 }
